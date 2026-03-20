f9b327f6fe9e3a142b2481b5cb84cd2cd0ed315c
AKD2028 <akd.tg2024@gmail.com>
2026-03-20 13:17:53 +0530

    First commit

---
+++ probe/probe.go
@@ -0,0 +1,58 @@
+package probe
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+type Result struct {
+	FileSize       int64
+	RangeSupported bool
+}
+
+func Probe(url string) (*Result, error) {
+
+	//HEAD-making head request
+	req, err := http.NewRequest("HEAD", url, nil)
+	if err != nil {
+
+		return nil, err
+	}
+
+	//Send the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("HTTP request failed:", err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	//server responded
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("server returned %s", resp.Status)
+	}
+
+	//Content-Length
+	lengthStr := resp.Header.Get("Content-Length")
+	if lengthStr == "" {
+		return nil, fmt.Errorf("no Content-Length header")
+	}
+
+	fileSize, err := strconv.ParseInt(lengthStr, 10, 64)
+	//just for safety
+	if err != nil {
+		return nil, err
+	}
+
+	//Accept-Range
+	rangeHeader := resp.Header.Get("Accept-Ranges")
+	rangeSupported := (rangeHeader == "bytes")
+
+	return &Result{
+		FileSize:       fileSize,
+		RangeSupported: rangeSupported,
+	}, nil
+
+}
